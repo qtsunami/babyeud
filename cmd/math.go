6468913249e946d27c3327ffb707dc8a996b2043
simple.xull <simple.xull@gmail.com>
2022-12-07 20:19:22 +0800

    one command

--- cmd/math.go
+++ cmd/math.go
@@ -2,4 +2,22 @@ package cmd
 
 import "github.com/spf13/cobra"
 
-var mathCmd = &cobra.Command{}
+var count int
+var start int
+var end int
+
+var mathCmd = &cobra.Command{
+	Use:   "math",
+	Short: "generate some math",
+	Long:  "",
+	Run: func(cmd *cobra.Command, args []string) {
+
+	},
+}
+
+func init() {
+	mathCmd.Flags().IntVarP(&count, "count", "c", 50, "Please input questions count.")
+	mathCmd.Flags().IntVarP(&start, "start", "s", 1, "Please input the range start number.")
+	mathCmd.Flags().IntVarP(&end, "end", "e", 50, "Please input the range end number.")
+
+}
